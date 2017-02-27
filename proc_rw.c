108ace3da4c8b39bcb2aa71de62fdb45af02155e
user <user@localhost.localdomain>
2017-02-27 09:30:31 +0900

    いろいろ修正

--- proc_rw.c
+++ proc_rw.c
@@ -1,10 +1,3 @@
-/*
- * sudo insmod proc_rw.ko 
- * ls /proc/hello 	//procファイル生成
- * echo unko > /proc/hello // procファイルへ書き込み
- * cat /proc/hello	// 読み込み
- */
-
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -14,92 +7,102 @@
 #include <linux/slab.h>
 #include "../kernel/sched/sched.h"
 #include <linux/rbtree.h>
+#include <linux/string.h>
+
+#define PRIO_CNT 40
 
 int len,temp;
-
+char prio[PRIO_CNT];
 char *msg;
 
-// モジュールAPIの登録
-struct file_operations proc_fops = {
-	owner:THIS_MODULE,
-	read: read_proc,
-	write: write_proc
-};
+char *make_str(char *pt, char *str)
+{
+	strcat(pt, str);
+	pt += sizeof(str);
+	return pt;
+}
 
-// ランキューの取得〜ルートノード取得
-struct rb_node *get_root(void){
-	
+// ランキューから、赤黒木のルートノードを取得
+struct rb_node *get_root(void)
+{
 	int cpu = smp_processor_id();
 	struct rq *rq = cpu_rq(cpu);
 	struct rb_node *root = (rq->cfs).tasks_timeline.rb_node;
 	return root;
 }
 
-// ノードに対応したタスク優先度取得
-void get_priority(struct rb_node *node){
+// ノードに対応したプロセスの優先度取得
+void get_priority(struct rb_node *node)
+{
 	
 	struct sched_entity *s_entity;
 	struct task_struct *task;
 
 	s_entity = rb_entry(node, struct sched_entity, run_node);	// sched_entry構造体取得
 	task = container_of(s_entity, struct task_struct, se);		// task構造体取得
-	printk( KERN_INFO "task_priority:%d", task->prio );
+	prio[task->static_prio - 100]++;							// 該当優先度のプロセス数をカウント
+	printk( KERN_INFO "task_priority:%d", task->static_prio );
 }
 
 // 二分木走査
-void rb_scan(struct rb_node *node){
+void rb_scan(struct rb_node *node)
+{
 	get_priority(node);
 	rb_scan(node->rb_left);
 	rb_scan(node->rb_right);
 }
 
+// msg にデータを書き出す
+// itoa() は使用できず、代替方法を探す必要あり
+void format_data(void)
+{
+	int i;
+	char *p;
+
+	make_str(msg, "priority\t\tcount\n");
+	for(i = 0; i < PRIO_CNT; i++){
+		if(prio[i])
+			make_str(p, itoa(i, NULL, DECIMAL));
+			p = make_str(p, "\t\t");
+			make_str(p, itoa(prio[i], NULL, DECIMAL));
+		}
+	}
+}
+
 // 処理の取りまとめ関数
-void count_process(void){
+void count_process(void)
+{
 	struct rb_node *root = get_root();
 	rb_scan(root);
 }
 
-
-int read_proc(struct file *filp,char *buf,size_t count,loff_t *offp ) 
+// cat /proc/miracle 時に呼ばれる
+ssize_t read_proc (struct file *fp, char __user *buf, size_t count, loff_t *ppos, int is_user)
 {
-	if(count>temp)
-	{
-		count=temp;
-	}
-	temp=temp-count;
-	copy_to_user(buf,msg, count);	// catのプロセス空間bufにデータコピー
-	if(count==0)					// ここで疑問。msgとprocファイルはどう結びついてるのか？
-		temp=len;
-
-	return count;
-}
-
-//write( fd, バッファ, サイズ)
-// ssize_t (*write) (struct file *, const char __user *, size_t, loff_t *);
-int write_proc(struct file *filp,const char *buf,size_t count,loff_t *offp)
-{
-	// copy_from_user: 
-	// ユーザ空間のデータをカーネル空間に転送
-	// copy_from_user( to, from, length)
-	copy_from_user(msg,buf,count);
-	len=count;
-	temp=len;
-	return count;
-}
-
-
-int proc_init (void) {
-	proc_create("hello",0666,NULL,&proc_fops);
-	
-	// モジュール読み込みの時点で、バッファを確保しておく
-	msg=kmalloc(GFP_KERNEL,10*sizeof(char));
-	
 	count_process();
+	format_data();
+	copy_to_user(buf, msg, strlen(msg));	// msg から プロセス空間の buf にデータコピー
+	return strlen(msg);
+}
+
+// モジュールAPIの登録
+struct file_operations proc_fops = {
+	owner:THIS_MODULE,
+	read: read_proc,
+	//write: write_proc
+};
+
+// 初期化関数
+int proc_init (void) 
+{
+	proc_create("miracle",0666,NULL,&proc_fops);	// /proc/sys/kernel 配下へ作成できず
+	msg=kmalloc(GFP_KERNEL,500*sizeof(char));		// ユーザ空間に渡すデータのバッファ
+	
 	return 0;
 }
 
 void proc_cleanup(void) {
-	remove_proc_entry("hello",NULL);
+	remove_proc_entry("miracle",NULL);
 }
 
 MODULE_LICENSE("GPL"); 
